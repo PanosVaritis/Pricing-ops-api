ed1ea791659fb071989746212d97d4419332dc91
Panagiotis Varitis <panosvaritis@gmail.com>
2025-12-20 10:57:11 +0200

    Added full code to take data from api and sent them to mino

--- azure_data.py
+++ azure_data.py
@@ -2,9 +2,10 @@ import requests
 from minio import Minio
 from minio.error import S3Error
 import json
-# from azure_urllib.parse import urlparse, parse_qs
+from urllib.parse import urlparse, parse_qs
 from config import azure_url,path, bucket, client
 from pathlib import Path
+
 
 try:
     if not client.bucket_exists(bucket):
@@ -43,31 +44,37 @@ except Exception as e:
 
 
 #For debug purposes
-print (azure_url)
+# print (azure_url)
 
-# while (data.get("NextPageLink")):
-# 
-#     azure_url = data.get("NextPageLink")
-#     print (azure_url)
-# 
-#     #Take query params from azure_url
-#     parsed = azure_urlparse(url)
-#     params = parse_qs(parsed.query)
-#     file_counter = params.get("$skip")[0]
-# 
-# 
-#     response = requests.get(azure_url)
+while (data.get("NextPageLink")):
 
-#     if response.status_code == 200:
-#         data = response.json ()
-#     else:
-#         print ("Error when trying to fetch data")
-# 
-#     file_name = f'{path}/azure_pricing_data_{file_counter}.json'
+    azure_url = data.get("NextPageLink")
+    # print (azure_url)
 
-#     with open (file_name, "w") as fi:
-#         json.dump(data, fi)
+    #Take query params from azure_url
+    parsed = urlparse(azure_url)
+    params = parse_qs(parsed.query)
+    file_counter = params.get("$skip")[0]
 
-# client.remove_object("azure", "pricing_data_0.json")
 
+    response = requests.get(azure_url)
+
+    if response.status_code == 200:
+        data = response.json ()
+    else:
+        print ("Error when trying to fetch data")
+
+    file_name = f'{path}/azure_pricing_data_{file_counter}.json'
+
+    try :
+        with open (file_name, "w") as fi:
+            json.dump(data, fi)
+
+        client.fput_object(bucket, Path(file_name).name, file_name)  #The first file name is the name that the file will have in minio and the second in the name locally
     
+    except S3Error as e:
+        print ("Error:",e)
+    
+    except Exception as e:
+        print ("Error:", e)
+
