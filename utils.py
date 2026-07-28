43233c6e01cef78252563641a1af37e476ec6e76
Panagiotis Varitis <panosvaritis@gmail.com>
2026-07-28 22:37:50 +0300

    Added method to extract all the serviceNames from azure

--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 from minio import Minio
 from minio.error import S3Error
 import logging
+import config
+import pandas as pd
 
 """
 This utility function takes a minio client and a possible bucket name as arguments, and
@@ -45,3 +47,54 @@ def set_up_logger():
             ]
         )
         logging.info("Logger initialized and activated.")
+
+
+
+"""
+This method extacts the unique services from Minio (serviceName from azure raw data).
+Its use is important so we can locate the ones we need and proceed with the EDA
+"""
+def find_service_name ():
+
+    set_up_logger()
+    logging.info ("Logger created, proceeding with client")
+    
+    client = config.create_minio_client()
+    logging.info ("Succesfully created minio client")
+
+
+    service_set = set()
+
+    objects = client.list_objects(config.PROVIDERS.get("azure").get("bucket"))
+
+    for obj in objects:
+        logging.info (f"Proccesing object: {obj.object_name}")
+
+        response = client.get_object(config.PROVIDERS.get("azure").get("bucket"), obj.object_name)
+
+        try:
+            df = pd.read_json(response)
+            df_flat = pd.json_normalize(df["Items"])
+
+            if 'serviceName' in df_flat.columns:
+                services = df_flat['serviceName'].dropna().unique()
+                service_set.update(services)
+
+        finally:
+            response.close()
+            response.release_conn()
+
+    logging.info(f"In tolal {len(service_set)} services were found")
+
+    for service in service_set:
+        logging.info(service)
+
+
+def main():
+    find_service_name()
+
+if __name__ == "__main__":
+    main()
+
+
+
